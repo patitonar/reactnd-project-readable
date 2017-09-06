22444169873ffd0858456c363ec563dd0ef564f2
Gerardo Nardelli <patitonardelli@gmail.com>
2017-09-06 20:44:05 -0300

    feat: add styles to default view

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import PostList from './PostList';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Route exact path='/' component={PostList} />
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Route exact path='/' component={PostList} />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- app/src/components/PostList.js
+++ app/src/components/PostList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts, fetchCategories } from '../actions';
+import {List, ListItem} from 'material-ui/List';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Divider from 'material-ui/Divider';
 
 class PostList extends Component {
   componentDidMount() {
@@ -12,12 +15,24 @@ class PostList extends Component {
     const { posts, categories } = this.props;
     return (
       <div>
-        <div>
-          {categories && categories.length > 0 && categories.map((categories, i) => (<p key={i}>{categories.name}</p>))}
-        </div>
-        <div>
-          {posts && posts.length > 0 && posts.map((post, i) => (<p key={i}>{post.title}</p>))}
-        </div>
+        <Tabs>
+          <Tab label="Home" />
+          {categories && categories.length > 0 && categories.map((categories, i) => (<Tab label={categories.name} key={i} />))}
+        </Tabs>
+          <List>
+            {posts 
+            && posts.length > 0 
+            && posts.map((post, i) => (
+              <div>
+                <ListItem 
+                  key={i} disabled
+                  primaryText={post.title}
+                  secondaryText={post.author}
+                />
+                <Divider inset={true} />
+              </div>
+            ))}
+          </List>
       </div>
     );
   }
