3e40909bffab4ff5c017f0b745f068f200af3e4f
Gerardo Nardelli <patitonardelli@gmail.com>
2017-10-03 19:14:15 -0300

    feat: display post information

--- app/src/components/GenericList.js
+++ app/src/components/GenericList.js
@@ -3,6 +3,7 @@ import List, { ListItem, ListItemText } from 'material-ui/List';
 import { Edit, DeleteForever } from 'material-ui-icons';
 import VoteScore from './VoteScore';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const GenericList = ({ items, handleDelete, handleVote }) => {
   return (
@@ -22,7 +23,9 @@ const GenericList = ({ items, handleDelete, handleVote }) => {
                     {item.title}
                   </Link>
                 }
-                secondary={`Author: ${item.author} - Comments: ${item.numComments}`}
+                secondary={`${moment(item.timestamp).format(
+                  'Do MMM YYYY'
+                )} - Author: ${item.author} - Comments: ${item.numComments}`}
               />
               <Edit />
               <DeleteForever onClick={() => handleDelete(item)} />
--- app/src/components/PostDetail.js
+++ app/src/components/PostDetail.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
+} from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import VoteScore from './VoteScore';
+import { Edit, DeleteForever } from 'material-ui-icons';
+import IconButton from 'material-ui/IconButton';
+import moment from 'moment';
 
 class PostDetail extends Component {
   render() {
     const { post } = this.props;
-    return <div>{post && `Hello ${post.id} from ${post.category}`}</div>;
+    return (
+      <div>
+        {post && (
+          <div>
+            <Card style={{ padding: 5, margin: 5 }}>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center'
+                }}
+              >
+                <VoteScore item={post} handleVote={() => {}} />
+                <CardHeader
+                  title={post.title}
+                  subheader={`Sent ${moment(post.timestamp).format(
+                    'Do MMMM YYYY, h:mm a'
+                  )} by ${post.author}`}
+                />
+                <div
+                  style={{
+                    flex: '1 1 auto'
+                  }}
+                />
+                <div>
+                  <Edit />
+                  <DeleteForever />
+                </div>
+              </div>
+              <CardContent>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add
+                  saffron and set aside for 10 minutes.
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
